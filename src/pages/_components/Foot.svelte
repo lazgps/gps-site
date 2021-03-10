c817594bf79ba3af1452f4b3fd1a03870a5bbf79
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-03-10 14:47:32 +0000

    fixed the location of assets

--- src/pages/_components/Foot.svelte
+++ src/pages/_components/Foot.svelte
@@ -3,22 +3,22 @@
     <div class="container flex flex-col items-center">
         <div class="grid grid-cols-2 grid-flow-row justify-items-auto md:mx-8 md:my-4 md:py-4 md:px-8">
             <div class="md:mr-6 md:pr-6">
-                <img class="h-12" src="../../assets/logos/equal-house-opp.png" alt="Equal Housing logo">
+                <img class="h-12" src="images/logos/equal-house-opp.png" alt="Equal Housing logo">
             </div>
             <div class="md:mr-6 md:pr-6">
-                <img class="h-12" src="../../assets/logos/florida-realtors.png" alt="Florida Realtors logo">
+                <img class="h-12" src="images/logos/florida-realtors.png" alt="Florida Realtors logo">
             </div>
             <div class="md:mr-6 md:pr-6">
-                <img class="h-12" src="../../assets/logos/narpm.png" alt="N.A.R.P.M. logo">
+                <img class="h-12" src="images/logos/narpm.png" alt="N.A.R.P.M. logo">
             </div>
             <div class="md:mr-6 md:pr-6">
-                <img class="h-12" src="../../assets/logos/ORRA.png" alt="O.R.R.A logo">
+                <img class="h-12" src="images/logos/ORRA.png" alt="O.R.R.A logo">
             </div>
             <div class="md:mr-6 md:pr-6">
-                <img class="h-12" src="../../assets/logos/realtor_logo.png" alt="Realtors logo">
+                <img class="h-12" src="images/logos/realtor_logo.png" alt="Realtors logo">
             </div>
             <div>
-                <img class="h-12" src="../../assets/logos/reos.png" alt="REOS logo">
+                <img class="h-12" src="images/logos/reos.png" alt="REOS logo">
             </div>
         </div>
     </div>
